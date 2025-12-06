708ebafc67f2a7115219c890fb912164789093f1
DanieleSalvi2810 <daniele.salvi.28@gmail.com>
2025-12-06 16:45:13 +0100

    added requirements for cuda and one submission

---
+++ req_cuda.sh
@@ -0,0 +1,10 @@
+python3 -m pip install --upgrade pip
+python3 -m pip insyall ipython
+python3 -m pip install nbformat
+python3 -m pip install numpy
+python3 -m pip install pandas
+python3 -m pip install pillow
+python3 -m pip install matplotlib
+python3 -m pip install scikit-learn
+python3 -m pip install jupytext
+python3 -m pip install torch==2.5.1+cu124 torchvision==0.20.1+cu124 torchaudio==2.5.1+cu124 --index-url https://download.pytorch.org/whl/cu124
