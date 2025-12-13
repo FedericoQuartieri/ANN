e24427b53b1fbd71e00f9b233a56b676998c4bab
FedericoQuartieri <federico.quartieri2002@gmail.com>
2025-12-13 14:18:39 +0100

    fixed error in preprocessing keys

--- download_nets.sh
+++ download_nets.sh
@@ -1,4 +1,4 @@
 cd torch/hub/checkpoints
 wget https://download.pytorch.org/models/resnet50-0676ba61.pth
-wget https://download.pytorch.org/models/efficientnet_b3_rwightman-cf984f9c.pth
+#wget https://download.pytorch.org/models/efficientnet_b3_rwightman-cf984f9c.pth
 wget https://download.pytorch.org/models/convnext_tiny-983f1562.pth
